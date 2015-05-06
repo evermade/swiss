ab4bf9b7b14e15425b9909f63d8564793fc56e81
Jaakko Alajoki <jaakko@evermade.fi>
2015-05-06 08:58:09 -0400

    Added check that init function exists.

--- assets/dist/main.all.js
+++ assets/dist/main.all.js
@@ -1,7 +1,7 @@
-var em={};$(document).ready(function(){for(var e in em)em[e].init()});
+var em={};$(document).ready(function(){for(var e in em)"function"==typeof em[e].init?em[e].init():console.error("Your block "+e+" doesn't define init-method.")});
 !function(){for(var o,e=function(){},n=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"],r=n.length,i=window.console=window.console||{};r--;)o=n[r],i[o]||(i[o]=e)}();
 !function(){em.base={},em.base.init=function(){em.base.example()},em.base.example=function(){}}();
-!function(){em.columns={},em.columns.init=function(){em.columns.example()},em.columns.example=function(){console.log("i got in!")}}();
+!function(){em.columns={},em.columns.init2=function(){em.columns.example()},em.columns.example=function(){console.log("i got in!")}}();
 !function(){em.block={},em.block.init=function(){em.block.example()},em.block.example=function(){}}();
 !function(){em.posts={},em.posts.init=function(){em.posts.example()},em.posts.example=function(){console.log("i got in!")}}();
 !function(){em.slider={},em.slider.init=function(){em.slider.setup()},em.slider.setup=function(){}}();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,23 @@
-var em = {}; //global var for all evermade modules 
+
+var em = {}; //global var for all evermade modules
 
 $(document).ready(function(){
 
-	//lets loop the em object for keys are call their init functions
+	// Let's loop the em object for keys are call their init functions.
 	for(var key in em){
-		em[key].init();
+
+		// Check if init function exists.
+		if (typeof(em[key].init) == "function") {
+
+			// Run init function.
+			em[key].init();
+
+		} else {
+
+			console.error("Your block " + key + " doesn't define init-method.");
+
+		}
+
 	}
+
 });
