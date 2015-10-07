edd9d0fe93ada93630d625df443cba0321f999c8
Paul Stewart <paul.stewart@evermade.fi>
2015-10-07 18:15:58 +0300

    Restrucctured the assest/scss with layouts, components and blocks

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php get_header();
 
-/* ------- FIRST GLANCE TEMPLATE -------- */
-include(locate_template('page-elements.php'));
-
-
-/* ------- REAL INDEX -------- */
-//include(locate_template('archive.php'));
+if(isset($_GET['toolbox'])){
+	/* ------- FIRST GLANCE TEMPLATE -------- */
+	include(locate_template('page-elements.php'));
+}
+else {
+	/* ------- REAL INDEX -------- */
+	include(locate_template('archive.php'));
+}
 
 get_footer();
 ?>
