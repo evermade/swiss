a541ddc7015f145c441cb052d251f7acb2dfd408
Paul Stewart <paul.stewart@evermade.fi>
2015-11-04 14:12:17 +0200

    updates

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 	</div><!-- end of .page-content -->
-</div><!-- end of .body -->
 
 <footer class="footer">
 	<div class="footer__container">
@@ -16,9 +15,4 @@
 	</div>
 </footer><!-- end of footer -->
 
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri();?>/assets/vendor/jquery/dist/jquery.min.js"><\/script>')</script>
-
-<?php wp_footer();?>
-</body>
-</html>
+<?php include(get_template_directory().'/templates/foot.php'); ?>
--- header.php
+++ header.php
@@ -1,32 +1,4 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title><?php bloginfo('name'); ?></title>
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<!--[if lt IE 9]>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
-	<![endif]-->
-
-	<?php wp_head();?>
-</head>
-<body <?php body_class(); ?> data-animate="animated fadeIn">
-
-  <!--[if lt IE 8]>
-    <div class="chromeframe">
-      You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.    </div>
-  <![endif]-->
-
-<div class="body">
-	<span id="top"></span>
+<?php include(get_template_directory().'/templates/head.php'); ?>
 	
 	<header class="main-header">
 		
