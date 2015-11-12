282c0ed1c38b2d77af6652112fd7a61a3d273092
Timo Sundvik <timo.sundvik@gmail.com>
2015-11-12 11:54:21 +0200

    Refactored the links-block to be a new hero component. Testing not completed.

--- templates/toolbox/blocks/table-div-links.php
+++ templates/toolbox/blocks/table-div-links.php
@@ -4,31 +4,29 @@ Tags: columns, layout, tables
 Preview: true
 -->
 
-<div class="table-div table-div--links">
+<div class="table-div">
 
 	<div class="table-div__row" data-count="3">
 
 	<?php for($i=0; $i<3; $i++): ?>
-		
+
 		<div class="table-div__col table-div__col--span1">
-			<div class="links-block__item">
+			<div class="hero">
 
-				<div class="links-block__item__background" style=" background-image:url(http://fakeimg.pl/650x650/eeeeee/666/?text=img);"></div>
+				<div class="hero__background" style=" background-image:url(http://fakeimg.pl/650x650/eee/666/?text=bg);"></div>
 
-				<div class="links-block__item__overlay"></div>
+				<div class="hero__overlay"></div>
 
-				<div class="links-block__item__content">
-					<h2>Component</h2>
-					<div>
-						<p>Within this block, we use the table div layout, and within this layout this would be a reusable component.</p>
-					</div>
-					<a href="1" class="btn">Read More</a>	
+				<div class="hero__content">
+					<h2>Default</h2>
+					<p>By default the hero has a fixed min-height. The content is centered horizontally and vertically.</p>
+					<a href="1" class="btn">Read More</a>
 				</div>
 
-			</div><!-- end of links block item -->
+			</div><!-- end of hero -->
 		</div><!-- end of table div col -->
 
-	<?php endfor; ?> 
+	<?php endfor; ?>
 
 	</div><!-- end of table div row -->
 
