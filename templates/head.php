4b19b680f323b121dbcb6dc8d3b985cfaac5bf50
Paul Stewart <paul.stewart@evermade.fi>
2015-11-17 14:32:11 +0200

    Updates to head for SEO

--- templates/head.php
+++ templates/head.php
@@ -6,8 +6,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title><?php bloginfo('name'); ?></title>
-	<meta name="description" content="">
+	<title><?php wp_title('|', true, 'right'); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<!--[if lt IE 9]>
