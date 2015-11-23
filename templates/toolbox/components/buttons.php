6a04d3d1a60018f5e28aba3d04da0acba9c6683d
Juha Lehtonen <juha@evermade.fi>
2015-11-23 10:19:16 +0200

    Added button examples to the toolbox

---
+++ templates/toolbox/components/buttons.php
@@ -0,0 +1,12 @@
+<!--
+Description: Different variations of our buttons, produced with different mixin variable combinations.
+Tags: button, buttons
+Preview: true
+-->
+
+
+<a class="btn" href="#">Normal button</a>
+
+<a class="btn btn--facebook" href="#">Button with left icon</a>
+
+<a class="btn btn--play-boxed" href="#">Button with right side icon inside a button-bg</a>
