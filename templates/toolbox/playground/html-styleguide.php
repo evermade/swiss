f366781393051d61893238472731299f5c034122
Joonas Pyhtilä <joonas.pyhtila@evermade.fi>
2016-04-05 15:16:56 +0300

    html styleguide added to playground

---
+++ templates/toolbox/playground/html-styleguide.php
@@ -0,0 +1,71 @@
+<section class="section">
+	<div class="container">
+
+		<div class="wysiwyg-html">
+
+			<h1>Heading 1</h1>
+
+			<p><strong>Paragraph of strong text, or lead paragraph. Lorem ipsum dolor sit amet, <a href="#">and here's a link within the text</a> tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</strong></p>
+			<p>Lorem ipsum dolor sit amet, <em>emphasized consectetur adipiscing elit</em>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+			<h2>Heading 2</h2>
+
+			<p>Lorem ipsum dolor sit amet, <a href="#">here's a link within the text</a> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+			<h3>Heading 3</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+			<h4>Heading 4</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+			<h5>Heading 5</h5>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+			<h6>Heading 6</h6>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+			<h3><a href="#">Heading with a link</a></h3>
+
+			<p>
+				<a href="#" class="btn">Button one</a>
+				<a href="#" class="btn">Button two</a>
+				<a href="#" class="btn">Button three</a>
+			</p>
+
+			<p>
+				<a href="#" class="btn">Single-button paragraph</a>
+			</p>
+
+			<p>
+				<a href="#" class="btn">Another one</a>
+			</p>
+
+			<ul>
+				<li>Unordered list <a href="#">item with link</a></li>
+				<li>Unordered list item</li>
+				<li>Unordered list item</li>
+			</ul>
+
+			<ol>
+				<li>Ordered list <a href="#">item with link</a></li>
+				<li>Ordered list item</li>
+				<li>Ordered list item</li>
+				<li>Ordered list item</li>
+				<li>Ordered list item</li>
+			</ol>
+
+			<dl>
+				<dt>Definition list topic <a href="#">with a link</a></dt>
+					<dd>Definition list definition</dd>
+				<dt>DL another item</dt>
+					<dd>DL another item's definition</dd>
+				<dt>DL one more item</dt>
+					<dd>DL one more item's definition</dd>
+			</dl>
+
+		</div>
+
+	</div><!-- end of example container -->
+</section><!-- end of example section-->
