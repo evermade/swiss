c911c35344a3908b45566c52657a35739bfb53de
Paul Stewart <onefastsnail@gmail.com>
2017-04-06 17:57:43 +0200

    fixed issue with body scroll when navigation is open

--- assets/js/components/page-navigation.js
+++ assets/js/components/page-navigation.js
@@ -1,28 +1,12 @@
 (function() {
 
-	em.pagenavigation = {};
+	em.pageNavigation = {};
 
-	em.pagenavigation.init = function(){
-		$(".c-mobile-toggle").on("click", function(){
-			$("body").toggleClass("js-navigation-open");
+	em.pageNavigation.init = function(){
+		$('.c-mobile-toggle').on('click', function(){
+			$('body').toggleClass('js-navigation-open');
 			return false;
 		});
-
-		var windowLastScrollPosition = 0;
-
-		if($(window).width() < 992){
-			$(window).scroll(function(){
-				if(windowLastScrollPosition > $(window).scrollTop()){
-					$('.b-page-navigation').removeClass("page-navigation--scroll-hidden");
-				} else if($(window).scrollTop() > $('.b-page-navigation').height()){
-					$('.b-page-navigation').addClass("page-navigation--scroll-hidden");
-				} else {
-					$('.b-page-navigation').removeClass("page-navigation--scroll-hidden");
-				}
-
-				windowLastScrollPosition = $(window).scrollTop();
-			});
-		}
 	};
 
 })();
