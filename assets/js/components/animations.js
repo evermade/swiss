fed9c392cc6fcd36a3e05f787ada8c96520d98f8
Evermade <pekka.wallenius@evermade.fi>
2017-07-19 22:10:40 +0300

    Refactored /components to use spaces

--- assets/js/components/animations.js
+++ assets/js/components/animations.js
@@ -1,91 +1,83 @@
 (function() {
 
-	//create empty object in the global em variable
-	em.animations = {
-		elements: {},
-		winWidthOk: false
-	};
+    // create empty object in the global em variable
+    em.animations = {
+        elements: {},
+        winWidthOk: false
+    };
 
-	//call any functions to be trigger on dom ready
-	em.animations.init = function(){
+    // call any functions to be trigger on dom ready
+    em.animations.init = function(){
+        em.animations.capture();
+        em.animations.checkRequiredWidth();
+        em.animations.animateFirstBlockIn();
+        em.animations.setup();
+    };
 
-		em.animations.capture();
+    em.animations.capture = function() {
+        em.animations.elements = $("[data-animate]");
+    };
 
-		em.animations.checkRequiredWidth();
+    em.animations.checkRequiredWidth = function() {
+        if(window.innerWidth>1024) {
+            em.animations.winWidthOk = true;
+            em.animations.animate();
+            em.animations.animateFirstBlockIn();
+        }
+        else {
+            em.animations.winWidthOk = false;
+        }
+    };
 
-		em.animations.animateFirstBlockIn();
+    em.animations.canWe = function(){
+        if(em.animations.elements.length && em.animations.winWidthOk === true) {
+            return true;
+        }
 
-		em.animations.setup();
+        return false;
+    };
 
-	};
+    em.animations.setup = function(){
 
-	em.animations.capture = function(){
-		em.animations.elements = $("[data-animate]");
-	};
+        $(window).on("scroll", function() {
 
-	em.animations.checkRequiredWidth = function(){
-		if(window.innerWidth>1024){
-			em.animations.winWidthOk = true;
-			em.animations.animate();
-			em.animations.animateFirstBlockIn();
-		}
-		else {
-			em.animations.winWidthOk = false;
-		}
+            if(!em.animations.canWe()) {
+                return false;
+            }
 
-	};
+            em.animations.animate();
 
-	em.animations.canWe = function(){
-		if(em.animations.elements.length && em.animations.winWidthOk === true) {
-			return true;
-		}
+        }).scroll();
+    };
 
-		return false;
-	};
+    em.animations.animate = function() {
+        em.animations.elements.each(function() {
 
-	em.animations.setup  = function(){
+            var $win = $(window),
+                $el = $(this),
+                scrollTop = $win.scrollTop(),
+                windowHeight = $win.height(),
+                elTop = $el.offset().top;
 
-		$(window).on("scroll", function() {
+            // el.toggleClass( el.data("animate"), elTop < (scrollTop+windowHeight));
 
-			if(!em.animations.canWe()){
-				return false;
-			}
+            if(elTop < (scrollTop+windowHeight)) {
+                $el.addClass($el.data("animate"));
+            }
+        });
+    };
 
-			em.animations.animate();
+    em.animations.animateFirstBlockIn = function() {
+        if(em.animations.canWe()) {
+            var $el = $('.main-header > section.hero + section');
+            var $container = $el.find('div').eq(0);
 
-		}).scroll();
-	};
-
-	em.animations.animate = function(){
-		em.animations.elements.each(function(){
-
-	        var $win = $(window),
-	        	$el = $(this),
-	        	scrollTop = $win.scrollTop(),
-	            windowHeight = $win.height(),
-	            elTop = $el.offset().top;
-
-	        // el.toggleClass( el.data("animate"), elTop < (scrollTop+windowHeight));
-
-	        if(elTop < (scrollTop+windowHeight)){
-	        	$el.addClass($el.data("animate"));
-	        }
-	    });
-	};
-
-	em.animations.animateFirstBlockIn = function(){
-		if(em.animations.canWe()){
-			var $el = $('.main-header > section.hero + section');
-			var $container = $el.find('div').eq(0);
-
-			if(!$el.hasClass('toBeAnimated')){
-				$container.addClass('animated fadeInUp');
-			}
-			else {
-				$container.css({opacity: 1});
-			}
-
-		}
-	};
-
+            if(!$el.hasClass('toBeAnimated')) {
+                $container.addClass('animated fadeInUp');
+            }
+            else {
+                $container.css({opacity: 1});
+            }
+        }
+    };
 })();
--- assets/js/components/forms.js
+++ assets/js/components/forms.js
@@ -1,17 +1,14 @@
 (function() {
 
-	em.forms = {};
+    em.forms = {};
 
-	em.forms.init = function(){
-		em.forms.events();
-	};
+    em.forms.init = function() {
+        em.forms.events();
+    };
 
-	em.forms.events = function(){
-
-		$('select.js-on-change-submit').change(function() {
+    em.forms.events = function() {
+        $('select.js-on-change-submit').change(function() {
             this.form.submit();
         });
-
-	};
-
+    };
 })();
