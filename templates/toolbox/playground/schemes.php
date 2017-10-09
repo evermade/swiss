daffbd6ba56726473fd996080a68929e01066a27
Paul Stewart <paul@onefastsnail.com>
2017-10-09 20:19:51 +0300

    Implemented #87 adding targeting to schemes. Using a data attribute to not contaminate the class list and to target elements that should be affected by the scheme. This may break legacy scheme stuff, so will fix in next commit. And yes empty data attributes are valid :P.

---
+++ templates/toolbox/playground/schemes.php
@@ -0,0 +1,75 @@
+<div class="scheme">
+    <section class="b-base">
+        <div class="b-base__container">
+
+            <div class="l-columns" data-column-count="2">
+
+                <div class="l-columns__item">
+                    <div class="h-wysiwyg-html" data-scheme-target>
+                        <h2>Targeted</h2>
+                        <p>This is a paragraph of text. Some of the text may be
+                            <em>emphasised</em> and some it might even be
+                            <strong>strongly emphasised</strong>. Occasionally
+                            <q>quoted text</q> may be found within a paragraph …and of course
+                            <a href="#">a link</a> may appear at any point in the text. The average paragraph contains five or six sentences
+                            although some may contain as little or one or two while others carry on for anything up to ten sentences
+                            and beyond.</p>
+                    </div>
+                </div>
+
+                <div class="l-columns__item">
+                    <div class="h-wysiwyg-html">
+                        <h2>Not Targeted</h2>
+                        <p>This is a paragraph of text. Some of the text may be
+                            <em>emphasised</em> and some it might even be
+                            <strong>strongly emphasised</strong>. Occasionally
+                            <q>quoted text</q> may be found within a paragraph …and of course
+                            <a href="#">a link</a> may appear at any point in the text. The average paragraph contains five or six sentences
+                            although some may contain as little or one or two while others carry on for anything up to ten sentences
+                            and beyond.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+</div>
+
+<div class="scheme scheme--inverted">
+    <section class="b-base">
+        <div class="b-base__container">
+
+            <div class="l-columns" data-column-count="2">
+
+                <div class="l-columns__item">
+                    <div class="h-wysiwyg-html" data-scheme-target>
+                        <h2>Targeted</h2>
+                        <p>This is a paragraph of text. Some of the text may be
+                            <em>emphasised</em> and some it might even be
+                            <strong>strongly emphasised</strong>. Occasionally
+                            <q>quoted text</q> may be found within a paragraph …and of course
+                            <a href="#">a link</a> may appear at any point in the text. The average paragraph contains five or six sentences
+                            although some may contain as little or one or two while others carry on for anything up to ten sentences
+                            and beyond.</p>
+                    </div>
+                </div>
+
+                <div class="l-columns__item">
+                    <div class="h-wysiwyg-html">
+                        <h2>Not Targeted</h2>
+                        <p>This is a paragraph of text. Some of the text may be
+                            <em>emphasised</em> and some it might even be
+                            <strong>strongly emphasised</strong>. Occasionally
+                            <q>quoted text</q> may be found within a paragraph …and of course
+                            <a href="#">a link</a> may appear at any point in the text. The average paragraph contains five or six sentences
+                            although some may contain as little or one or two while others carry on for anything up to ten sentences
+                            and beyond.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+</div>
