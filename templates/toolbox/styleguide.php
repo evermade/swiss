4d941b80e810d9983048b78ab6ead2c3f3b5b004
Paul Stewart <paul@onefastsnail.com>
2017-10-10 14:31:06 +0300

    Fixed btn link color bug when in wysiwyg-html context. Fixed scheming in toolbox.

--- templates/toolbox/styleguide.php
+++ templates/toolbox/styleguide.php
@@ -1,8 +1,8 @@
 <section class="b-toolbox">
     <div class="b-toolbox__container">
-        <div id="js-toolbox-scheme-target" class="b-toolbox__scheme-wrapper scheme">
-            <a href="#" class="c-btn js-toolbox-switch" data-activate-scheme="scheme">Default</a>
-            <a href="#" class="c-btn js-toolbox-switch" data-activate-scheme="scheme--inverted">Inverted</a>
+        <div id="js-toolbox-scheme-target" class="b-toolbox__scheme-wrapper s-context">
+            <a href="#" class="c-btn js-toolbox-switch" data-activate-scheme="s-context">Default</a>
+            <a href="#" class="c-btn js-toolbox-switch" data-activate-scheme="s-context--inverted">Inverted</a>
             <br><br>
             <h5>h-wysiwyg-html<strong> -wrapper adds margin around each html element making articles look beautiful.</strong></h5>
             <hr>
