88d77db9bac93c546f70f5fb3c9ab3f7db6007e6
Paul Stewart <paul@onefastsnail.com>
2017-10-10 10:34:56 +0300

    Fixed scheme target in styleguide

--- templates/toolbox/styleguide.php
+++ templates/toolbox/styleguide.php
@@ -7,18 +7,9 @@
             <h5>h-wysiwyg-html<strong> -wrapper adds margin around each html element making articles look beautiful.</strong></h5>
             <hr>
             <br><br>
-            <div class="h-wysiwyg-html">
+            <div class="h-wysiwyg-html" data-scheme-target>
                 <?php include(get_template_directory().'/templates/_style-guide.php'); ?>
             </div>
         </div>
-        <?php /*
-        <br /><br /><br /><br />
-
-        <div>
-            <h1>Without .h-wysiwyg-html</h1>
-            <hr><br>
-            <?php include(get_template_directory().'/templates/_style-guide.php'); ?>
-        </div>
-        */ ?>
     </div>
 </section>
