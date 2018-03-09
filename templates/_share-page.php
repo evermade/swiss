bd1ab9c40478401820e35e143462f695bacd85cf
Timo Sundvik <timo.sundvik@gmail.com>
2018-03-09 14:10:48 +0200

    Removed colon from share title.

--- templates/_share-page.php
+++ templates/_share-page.php
@@ -1,5 +1,5 @@
 <div class="c-share">
-    <h6 class="c-share__title"><?php _e('Share:', 'swiss'); ?></h6>
+    <h6 class="c-share__title"><?php _e('Share', 'swiss'); ?></h6>
     <ul class="c-share__list">
         <?php foreach ($services as $key => $value): ?>
             <li>
