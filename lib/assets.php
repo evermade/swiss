bd7a8cbb95fe498d18c7a176f8aa5c3ef807378b
Evermade <pekka.wallenius@evermade.fi>
2018-09-24 15:31:51 +0300

    Added Webpack 4 to theme and amended assets folder structure
    Webpack was added to theme to properly separate the Docker stack
    from the Swiss theme. Assets folder structure has been amended
    to support non-CSS/JS assets better.
    
    NOTE! /assets/src is not going to be available on staging/production
    so don't link to anything inside that folder.
    
    - /assets/src is for stuff that needs to be compiled / preprocessed
    - /assets/dist is for the stuff that will be actually served

--- lib/assets.php
+++ lib/assets.php
@@ -13,14 +13,14 @@ function publicScriptsAndStyles()
     // scripts
     wp_enqueue_script('modernizr', '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js');
 
-    wp_enqueue_script('swiss-js', get_template_directory_uri().'/assets/dist/js/bundle.'.filemtime(get_stylesheet_directory() . '/assets/dist/js/bundle.js').'.js', array(), null, true);
+    wp_enqueue_script('swiss', get_template_directory_uri().'/assets/dist/main.'.filemtime(get_stylesheet_directory() . '/assets/dist/main.js').'.js', array(), null, true);
 
     // fonts
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto:400,400italic,300,100italic,100,300italic,500,500italic,700,700italic,900,900italic');
     wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.0.6/css/all.css');
 
     // styles
-    wp_enqueue_style('swiss-css', get_template_directory_uri().'/assets/dist/css/bundle.'.filemtime(get_stylesheet_directory() . '/assets/dist/css/bundle.css').'.css');
+    wp_enqueue_style('swiss', get_template_directory_uri().'/assets/dist/main.'.filemtime(get_stylesheet_directory() . '/assets/dist/main.css').'.css');
 }
 
 add_action('wp_enqueue_scripts', 'Evermade\Swiss\Assets\publicScriptsAndStyles');
